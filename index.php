984b0be04c101cfa0c09c318381d636a9646beca
Peter Muehlbacher <muehlbacher.peter@gmail.com>
2012-06-01 16:07:09 +0200

    imported new files (CSS and A*)

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html>
 <head>
     <title>HTML5 Tower Defense</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css" media="all"/>
 </head>
 <body>
     <canvas id="game" width="800" height="600"></canvas>
     
+    <script type="text/javascript" src="js/lib/a_star.js"></script>
     <script type="text/javascript" src="js/lib/kibo.min.js"></script>
     <script type="text/javascript" src="js/game.js"></script>
 </body>
